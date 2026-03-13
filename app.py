76c40dbee3e9f072d247c764051bad116c5450e4
JaeChul <Alocke918@proton.me>
2026-03-13 11:38:06 +0900

    Added another page (so 3 in total). Added features page

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 
-def index():
-    return "Index:"
-
-@app.route('/home')
-
 def home():
-    return "Home:"
+    return "Home: PlaceHolder for now"
+
+@app.route('/about')
+
+def about():
+    return "This is an open-source News Aggregator developed by Yours Truly"
+
+@app.route('/features')
+
+def features():
+    return "Features will go here maybe/ HTML Template Rendering will be added"
 
 if __name__ == '__main__':
     app.run(debug=True)
